e013945a4ed16b664cd7f084ba1d47966b855cd0
Ganesh Katrapati <ganesh.katrapati@research.iiit.ac.in>
2014-07-23 21:58:38 +0530

    Small changes

--- nlgo_main.go
+++ nlgo_main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/neshkatrapati/nlgo/ngrams"
+	"os"
+	"strconv"
 )
 
 func main() {
-	ng := ngrams.NewNGrams(2)
+
+	var NS string = os.Args[1] // Give N as command line arg
+	N, _ := strconv.Atoi(NS)
+	ng := ngrams.NewNGrams(N)
 	var String string = "I am sam sam I am I do not eat ham"
-	ng.FromString(String, " ")
 	fmt.Println(String)
+	ng.FromString(String, " ")
+
 	for _, ngram := range ng.Grams {
 		fmt.Println(ngram)
 	}
